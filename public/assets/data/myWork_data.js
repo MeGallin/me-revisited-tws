4ebf4cb40478bd531bcfe26c42aacb425180189d
MeGallin <me@garyallin.uk>
2023-01-30 11:15:03 +0000

    my work comp

---
+++ public/assets/data/myWork_data.js
@@ -0,0 +1,33 @@
+export const myWorkData = [
+  {
+    title: 'Your Corporate Memory',
+    techStack: 'MERN',
+    github: 'https://github.com/MeGallin/new-corporate-memory-tws',
+    url: 'https://yourcorporatememory.com',
+    description:
+      'A productivity tool designed to help you save mundane tasks and and information. ',
+    features: [
+      'Custom Authentication-register-login-reset functionality.',
+      'Private user admin area and dashboard.',
+      'Private admin dashboard for ADMIN showing USER stats and allowing ADMIN to control.',
+      'Setup email reminders',
+      'Upload images with edit and delete functionality.',
+      'Add and delete tags to each memory.',
+      'Search for memories by title or tag.',
+    ],
+  },
+  {
+    title: 'Club Manager',
+    techStack: 'MERN',
+    github: 'https://github.com/MeGallin/new-corporate-memory-tws',
+    url: 'https://yourcorporatememory.com',
+    description:
+      'A productivity tool aimed at people running social clubs. djdjjd riiri',
+    features: [
+      'Custom Authentication-register-login-reset functionality.',
+      'Private user admin area and dashboard.',
+      'Private admin dashboard for ADMIN showing USER stats and allowing ADMIN to control.',
+      'Admin and USER can send private messages.',
+    ],
+  },
+];
---
+++ src/Components/MyWorkCard/MyWorkCardComponent.jsx
@@ -0,0 +1,71 @@
+import { useId } from 'react';
+import './MyWorkCardComponent.css';
+import PropTypes from 'prop-types';
+
+const MyWorkCardComponent = ({ options }) => {
+  //   console.log(options);
+  const id = useId();
+  return options.map((option, i) => (
+    <div key={`${id}-option-${i}`} className="my_work_card_wrapper">
+      <div>
+        <span className="my_work_card_title">
+          <sup>TITLE:</sup>
+        </span>
+        <div>
+          <h2>{option.title}</h2>
+        </div>
+      </div>
+      <div>
+        <span className="my_work_card_title">
+          <sup>TECH-STACK:</sup>
+        </span>
+        <p>{option.techStack}</p>
+      </div>
+      <div>
+        <span className="my_work_card_title">
+          <sup>URL:</sup>
+        </span>
+        <p>
+          <a href={option.url} target="_blank" rel="noreferrer">
+            {option.title}
+          </a>
+        </p>
+      </div>
+      <div>
+        <span className="my_work_card_title">
+          <sup>GITHUB:</sup>
+        </span>
+        {option?.github.map((link, i) => (
+          <p key={`${id}-github-${i}`}>
+            <a href={link} target="_blank" rel="noreferrer">
+              {option.title}
+            </a>
+          </p>
+        ))}
+      </div>
+
+      <div>
+        <span className="my_work_card_title">
+          <sup>DESCRIPTION:</sup>
+        </span>
+        <p>{option.description}</p>
+      </div>
+      <div>
+        <span className="my_work_card_title">
+          <sup>FEATURES:</sup>
+        </span>
+        {option.features.map((feature, i) => (
+          <div key={`${id}-feature-${i}`}>
+            <p>{JSON.stringify(feature).replace(/["']/g, '')}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  ));
+};
+
+MyWorkCardComponent.propTypes = {
+  id: PropTypes.string,
+};
+
+export default MyWorkCardComponent;
