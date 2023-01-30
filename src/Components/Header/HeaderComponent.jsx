9e03151fdb995cc86e98a905a1f3b1e1b056ffa2
MeGallin <me@garyallin.uk>
2023-01-30 13:46:30 +0000

    added beta release

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -8,6 +8,7 @@ import LogoComponent from '../Logo/LogoComponent';
 import TellMeComponent from '../TellMe/TellMeComponent';
 
 import { logoutAction } from '../../Store/Actions/userActions';
+import BetaReleaseComponent from '../BetaRelease/BetaReleaseComponent';
 
 const HeaderComponent = () => {
   const dispatch = useDispatch();
@@ -23,91 +24,94 @@ const HeaderComponent = () => {
   };
 
   return (
-    <header>
-      <nav>
-        <LogoComponent />
-        <div
-          className="hamburger"
-          onClick={() => setToggleHamburger((prev) => (prev = !prev))}
-        >
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
-        </div>
+    <>
+      <BetaReleaseComponent />
+      <header>
+        <nav>
+          <LogoComponent />
+          <div
+            className="hamburger"
+            onClick={() => setToggleHamburger((prev) => (prev = !prev))}
+          >
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
 
-        <ul className={!toggleHamburger ? 'nav_links' : 'nav_links open'}>
-          <li className="link">
-            <NavLink
-              onClick={() => setToggleHamburger((prev) => (prev = !prev))}
-              className={(navData) => (navData.isActive ? 'active' : '')}
-              to="/"
-            >
-              <TellMeComponent text={['H', 'ome']} />
-            </NavLink>
-          </li>
-          <li className="link">
-            <NavLink
-              onClick={() => setToggleHamburger((prev) => (prev = !prev))}
-              className={(navData) => (navData.isActive ? 'active' : '')}
-              to="/about"
-            >
-              <TellMeComponent text={['ab', 'out']} />
-            </NavLink>
-          </li>
-          <li className="link">
-            <NavLink
-              onClick={() => setToggleHamburger((prev) => (prev = !prev))}
-              className={(navData) => (navData.isActive ? 'active' : '')}
-              to="/my-work"
-            >
-              <TellMeComponent text={['m', 'y', ' w', 'o', 'rk']} />
-            </NavLink>
-          </li>
-          <li className="link">
-            <NavLink
-              onClick={() => setToggleHamburger((prev) => (prev = !prev))}
-              className={(navData) => (navData.isActive ? 'active' : '')}
-              to="/contact"
-            >
-              <TellMeComponent text={['con', 't', 'act']} />
-            </NavLink>
-          </li>
+          <ul className={!toggleHamburger ? 'nav_links' : 'nav_links open'}>
+            <li className="link">
+              <NavLink
+                onClick={() => setToggleHamburger((prev) => (prev = !prev))}
+                className={(navData) => (navData.isActive ? 'active' : '')}
+                to="/"
+              >
+                <TellMeComponent text={['H', 'ome']} />
+              </NavLink>
+            </li>
+            <li className="link">
+              <NavLink
+                onClick={() => setToggleHamburger((prev) => (prev = !prev))}
+                className={(navData) => (navData.isActive ? 'active' : '')}
+                to="/about"
+              >
+                <TellMeComponent text={['ab', 'out']} />
+              </NavLink>
+            </li>
+            <li className="link">
+              <NavLink
+                onClick={() => setToggleHamburger((prev) => (prev = !prev))}
+                className={(navData) => (navData.isActive ? 'active' : '')}
+                to="/my-work"
+              >
+                <TellMeComponent text={['m', 'y', ' w', 'o', 'rk']} />
+              </NavLink>
+            </li>
+            <li className="link">
+              <NavLink
+                onClick={() => setToggleHamburger((prev) => (prev = !prev))}
+                className={(navData) => (navData.isActive ? 'active' : '')}
+                to="/contact"
+              >
+                <TellMeComponent text={['con', 't', 'act']} />
+              </NavLink>
+            </li>
 
-          {userInfo || googleUserInfo ? (
-            <>
-              <li className="link">
-                <NavLink
-                  onClick={() => setToggleHamburger((prev) => (prev = !prev))}
-                  className={(navData) => (navData.isActive ? 'active' : '')}
-                  to="/dashboard"
-                >
-                  <TellMeComponent text={['dashBoard']} />
-                </NavLink>
-              </li>
-              <ButtonComponent
-                type="submit"
-                text="logout"
-                variant="dark"
-                onClick={handleLogout}
-                disabled={false}
-              />
-            </>
-          ) : (
-            <>
-              <li className="link">
-                <NavLink
-                  onClick={() => setToggleHamburger((prev) => (prev = !prev))}
-                  className={(navData) => (navData.isActive ? 'active' : '')}
-                  to="/cv"
-                >
-                  <TellMeComponent text={['c', 'V']} />
-                </NavLink>
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
-    </header>
+            {userInfo || googleUserInfo ? (
+              <>
+                <li className="link">
+                  <NavLink
+                    onClick={() => setToggleHamburger((prev) => (prev = !prev))}
+                    className={(navData) => (navData.isActive ? 'active' : '')}
+                    to="/dashboard"
+                  >
+                    <TellMeComponent text={['dashBoard']} />
+                  </NavLink>
+                </li>
+                <ButtonComponent
+                  type="submit"
+                  text="logout"
+                  variant="dark"
+                  onClick={handleLogout}
+                  disabled={false}
+                />
+              </>
+            ) : (
+              <>
+                <li className="link">
+                  <NavLink
+                    onClick={() => setToggleHamburger((prev) => (prev = !prev))}
+                    className={(navData) => (navData.isActive ? 'active' : '')}
+                    to="/cv"
+                  >
+                    <TellMeComponent text={['c', 'V']} />
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
