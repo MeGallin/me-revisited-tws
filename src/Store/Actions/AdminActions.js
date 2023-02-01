b81834987f5ed383df5bee60207c0075561c0669
MeGallin <me@garyallin.uk>
2023-02-01 09:41:23 +0000

    delete working

--- src/Store/Actions/AdminActions.js
+++ src/Store/Actions/AdminActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import {
+  ADMIN_USER_DELETE_FAILURE,
+  ADMIN_USER_DELETE_REQUEST,
+  ADMIN_USER_DELETE_SUCCESS,
   ADMIN_USER_DETAILS_FAILURE,
   ADMIN_USER_DETAILS_REQUEST,
   ADMIN_USER_DETAILS_SUCCESS,
@@ -39,3 +42,37 @@ export const adminUserDetailsAction = () => async (dispatch, getState) => {
     });
   }
 };
+
+//DELETE: ADMIN delete all USER's data
+export const adminDeleteAllUserDataAction =
+  (id) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ADMIN_USER_DELETE_REQUEST,
+      });
+      const {
+        userLogin: { userInfo },
+      } = getState();
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.delete(
+        `${process.env.REACT_APP_END_POINT}api/admin/user-delete/${id}`,
+        config,
+      );
+      dispatch({ type: ADMIN_USER_DELETE_SUCCESS, payload: data });
+      dispatch(adminUserDetailsAction());
+    } catch (error) {
+      dispatch({
+        type: ADMIN_USER_DELETE_FAILURE,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
